deeccc472de5c9cc31004ae9e8d89a59ede01106
Houssem KALLEL <h.kallel@reply.de>
2018-11-08 18:39:53 +0100

    Create citizen custom extension

---
+++ hybris/bin/custom/training/trainingfulfilmentprocess/gensrc/de/hybris/training/fulfilmentprocess/constants/GeneratedTrainingFulfilmentProcessConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 8, 2018 11:10:09 AM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of SAP
+ * Hybris ("Confidential Information"). You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with SAP Hybris.
+ *  
+ */
+package de.hybris.training.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "trainingfulfilmentprocess";
+	
+	protected GeneratedTrainingFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
